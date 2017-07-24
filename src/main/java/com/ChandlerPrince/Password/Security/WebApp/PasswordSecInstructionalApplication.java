01dcb9bc73d281139ca241f0afdf2bee4b95c9f7
dchandlerp <chandler.prince@gmail.com>
2017-07-24 11:26:56 -0500

    Initial commit

---
+++ src/main/java/com/ChandlerPrince/Password/Security/WebApp/PasswordSecInstructionalApplication.java
@@ -0,0 +1,12 @@
+package com.ChandlerPrince.Password.Security.WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordSecInstructionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordSecInstructionalApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ChandlerPrince/Password/Security/WebApp/PasswordSecInstructionalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ChandlerPrince.Password.Security.WebApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PasswordSecInstructionalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
