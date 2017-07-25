b63e19b838e9557ec4c94e0abb499267cafb1948
dchandlerp <chandler.prince@gmail.com>
2017-07-25 14:26:51 -0500

    Added @Transactional and @Repository too my DAO

--- src/main/java/model/data/PasswordDAO.java
+++ src/main/java/model/data/PasswordDAO.java
@@ -2,10 +2,15 @@ package model.data;
 
 import model.PasswordEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 /**
  * Created by davidprince on 7/25/17.
  */
+@Transactional
+@Repository
 public interface PasswordDAO extends CrudRepository<PasswordEntity, Integer> {
 
     PasswordEntity findByPassword(String password);
