06c2641cf1611a6dc05f3fd20ceac0cc6ee2ef5f
dchandlerp <chandler.prince@gmail.com>
2017-07-25 13:36:05 -0500

    Created FindByPassword

--- src/main/java/model/data/PasswordDAO.java
+++ src/main/java/model/data/PasswordDAO.java
@@ -7,4 +7,6 @@ import org.springframework.data.repository.CrudRepository;
  * Created by davidprince on 7/25/17.
  */
 public interface PasswordDAO extends CrudRepository<PasswordEntity, Integer> {
+
+    PasswordEntity findByPassword(String password);
 }
